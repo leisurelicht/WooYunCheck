6bf7ef6df0cf76a7ea73a70591b5b03bb7b2b4c4
暮晨 <leisurelylicht@foxmail.com>
2015-08-23 20:02:05 +0800

    Merge branch 'master' into test
    增加函数功能说明

--- CheckWooYun.py
+++ CheckWooYun.py
@@ -38,6 +38,9 @@ class WooYun(object):
         print self.name,"is over"
 
     def keyWordsread(self,keyfile):
+        '''
+        从文件中读取需要监看的关键字
+        '''
         if os.path.exists(keyfile):
             for key in open (keyfile,'r'):
                 newKey =  key.strip()
@@ -66,13 +69,15 @@ class WooYun(object):
             self.keyWordscheck(text)
 
     def keyWordscheck(self,text):
+        '''
+        检查获得的标题中是否含有要监看的关键字
+        '''
         print self.name,"is start keyWordscheck in",self.count
 
         try :
             #raise Exception("data is not json")
             data = json.loads(text)
         except Exception as e :
-            #print e
             text = "Error in function : \" %s \" ,\n \
             Error name is : \" %s \" ,\n \
             Error type is : \" %s \" ,\n \
@@ -86,18 +91,17 @@ class WooYun(object):
                 temp_name = data[i].get('title')
                 temp_url = data[i].get('link')
                 temp_id = data[i].get('id')
-                #print temp_name
                 for Key in self.keyWordslist:
-                    #print Key,"===",
-                    #print temp_name.find(Key)
                     if ( temp_name.find(Key) != -1 ):
-                        #print "ready send"
                         self.sendRecord(temp_name+self.website,temp_url,temp_id)
                         break
         self.count += 1
 
 
     def sendRecord(self,title,url,id):
+        '''
+        调用邮件初始化函数并记录被发送的事件ID
+        '''
         print self.name,"is start sendRecord in",self.count
 
         temp = []
@@ -130,25 +134,36 @@ class WooYun(object):
             print "Same thing was sent,did not send same mail to everyone"
 
     def mailInit(self,title,message,messagetype):
+        '''
+        初始化邮件设置
+        title: 邮件标题
+        message: 邮件正文,在不同类型的邮件中内容不同
+        messagetype有三个值:
+        securityinfo: 发送监看事件时调用
+        exceptionInfo: 发送错误报告时调用
+        timereport: 发动定时运行报告时调用
+        '''
         print self.name,"is start mailInit in",self.count
 
         sender = '3118706739@qq.com'  #发件人
         #receiver = ['leisurelylicht@126.com','zhao_haixu@venustech.com.cn','liuchang@venustech.com.cn','zhang_dejun@venustech.com.cn'] #收件人
         receiver = ['leisurelylicht@126.com'] #测试
         receiver_admin = 'leisurelylicht@126.com'
-        subject = title  #邮件标题
         smtpserver = 'smtp.qq.com'  #邮件服务器
         username = '3118706739@qq.com'  #邮箱登录名
         password = self.mailpassword  #邮箱登陆密码
 
         param = {'sender':sender,'receiver':receiver,\
-        'subject':subject,'smtpserver':smtpserver,\
+        'subject':title,'smtpserver':smtpserver,\
         'username':username,'password':password,\
         'receiver_admin':receiver_admin}
 
         self.sendEmail(message,param,messagetype)
 
     def sendEmail(self,message,param,messagetype):
+        '''
+        发送邮件
+        '''
         print self.name,"is start sendSecurity in",self.count
 
         msg = MIMEText(message,'text')#中文参数‘utf-8’，单字节字符不需要
@@ -187,6 +202,7 @@ if __name__ == '__main__':
     Baojianhui = WooYun('WooYun保监会',mailpassword,'Baojianhui.txt', wooyun_url)
     jijin = WooYun('WooYun基金',mailpassword,'jijin.txt',wooyun_url)
     yinhang = WooYun('WooYun银行',mailpassword,'yinhang.txt',wooyun_url)
+
     timereport = WooYun('WooYun运行报告',mailpassword)
     timereport.mailInit('running report from WooYun','program start running',"timereport")
 
@@ -197,6 +213,7 @@ if __name__ == '__main__':
             timereport.mailInit('running report from WooYun','program is running',"timereport")
             one = two
             print "Scheduled connections was sent"
+
         Guoziwei.dataRequest()
         Baojianhui.dataRequest()
         jijin.dataRequest()
