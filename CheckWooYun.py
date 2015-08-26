232923d63beec42c4da3b710869b942201a7e49f
暮晨 <362864550@qq.com>
2015-08-26 13:18:32 +0800

    修改注释

--- CheckWooYun.py
+++ CheckWooYun.py
@@ -69,8 +69,8 @@ class WooYun(object):
         '''
         print self.name,"is start dataRequest in",self.count
         try:
-            #text = requests.get(self.check_url).content
-            raise Exception("connect error")
+            text = requests.get(self.check_url).content
+            #raise Exception("connect error")
         except (requests.exceptions.ConnectionError,Exception) as e :
                 #print e
                 text = "Error in function : \" %s \" ,\n \
@@ -118,6 +118,8 @@ class WooYun(object):
     def sendRecord(self,title,url,id):
         '''
         调用邮件初始化函数并记录被发送的事件ID
+        没有返回值
+        函数内调用mailInit()
         '''
         print self.name,"is start sendRecord in",self.count
 
@@ -159,6 +161,8 @@ class WooYun(object):
         securityinfo: 发送监看事件时调用
         exceptionInfo: 发送错误报告时调用
         timereport: 发动定时运行报告时调用
+        没有返回值
+        函数内调用sendEmail()
         '''
         print self.name,"is start mailInit in",self.count
 
@@ -179,12 +183,15 @@ class WooYun(object):
     def sendEmail(self,message,param,messagetype):
         '''
         发送邮件
+        没有返回值
+        函数内调用_format_addr()
         '''
         print self.name,"is start sendEmail in",self.count
 
         msg = MIMEText(message,'plain','utf-8')#中文参数‘utf-8’，单字节字符不需要
         #msg = MIMEText('hello wold','text')
-        msg['Subject'] = Header(param['subject'])
+        msg[ 'From' ] = self._format_addr( u'WooYun监看机器人<%s>' % param['sender'] )
+        msg[ 'Subject' ] = Header( param['subject'] )
 
         try:
             smtp = smtplib.SMTP( param['smtpserver'] , 25 )
@@ -192,8 +199,10 @@ class WooYun(object):
             #smtp.set_debuglevel(1)
             smtp.login(param['username'],param['password'])
             if (messagetype == "securityinfo"):
+                msg[ 'To' ] = self._format_addr(u'Dollars<%s>' % param['receiver'] )
                 smtp.sendmail(param['sender'],param['receiver'],msg.as_string())
             else:
+                msg[ 'To' ] = self._format_addr(u'Admin<%s>' % param['receiver_admin'] )
                 smtp.sendmail(param['sender'],param['receiver_admin'],msg.as_string())
 
         except Exception as e :
@@ -208,6 +217,15 @@ class WooYun(object):
         else:
             smtp.quit()
 
+    def _format_addr(self,s):
+        '''
+        格式化一个邮件地址
+        返回一个被格式化为 '别名<email address>' 的邮件地址
+        '''
+        name , addr = parseaddr(s)
+        return formataddr(( Header(name,'utf-8').encode(),\
+                          addr.encode('utf-8') if isinstance(addr,unicode) else addr ))
+
 if __name__ == '__main__':
     count = 0
     one = time.time() #开始时间
@@ -221,20 +239,20 @@ if __name__ == '__main__':
     yinhang = WooYun('WooYun银行',mailpassword,'yinhang.txt',wooyun_url)
 
     timereport = WooYun('WooYun运行报告',mailpassword)
-    timereport.mailInit('running report','program start running',"timereport")
+    timereport.mailInit('Running report','Program start running',"timereport")
 
     while True:
         print "system is running in [",count,"],now is",time.ctime()
         two = time.time() #当前时间
-        if ( two - one ) > 10:
-            timereport.mailInit('running report from WooYun','program is running',"timereport")
+        if ( two - one ) > 1800:
+            timereport.mailInit('Running report','Program is running',"timereport")
             one = two
             print "Scheduled connections was sent"
 
         Guoziwei.dataRequest()
-        #Baojianhui.dataRequest()
-        #jijin.dataRequest()
-        #yinhang.dataRequest()
+        Baojianhui.dataRequest()
+        jijin.dataRequest()
+        yinhang.dataRequest()
         print "This cycle [",count,"] was end in",time.ctime()
         count += 1
-        time.sleep(5)
+        time.sleep(300)
